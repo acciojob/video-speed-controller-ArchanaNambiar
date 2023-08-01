a2430488b3e8be1d113c7ed73500f6fee87b7345
ArchanaNambiar <64783368+ArchanaNambiar@users.noreply.github.com>
2023-08-01 20:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
-const inputs = document.querySelectorAll('.controls input');
+// Get the video player and all required elements
+const videoPlayer = document.querySelector('.player__video');
+const progress = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
+const playButton = document.querySelector('.player__button');
+const volumeInput = document.querySelector('.player__slider[name="volume"]');
+const playbackSpeedInput = document.querySelector('.player__slider[name="playbackRate"]');
+const skipButtons = document.querySelectorAll('[data-skip]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+// Add event listeners for the required elements
+playButton.addEventListener('click', togglePlay);
+videoPlayer.addEventListener('click', togglePlay);
+videoPlayer.addEventListener('play', updatePlayButton);
+videoPlayer.addEventListener('pause', updatePlayButton);
+videoPlayer.addEventListener('timeupdate', updateProgress);
+progress.addEventListener('click', scrub);
+volumeInput.addEventListener('input', updateVolume);
+playbackSpeedInput.addEventListener('input', updatePlaybackSpeed);
+skipButtons.forEach(button => button.addEventListener('click', skip));
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+// Function to toggle play and pause
+function togglePlay() {
+  if (videoPlayer.paused) {
+    videoPlayer.play();
+  } else {
+    videoPlayer.pause();
+  }
+}
+
+// Function to update the play button icon
+function updatePlayButton() {
+  const icon = videoPlayer.paused ? '►' : '❚ ❚';
+  playButton.textContent = icon;
+}
+
+// Function to update the progress bar
+function updateProgress() {
+  const progressPercent = (videoPlayer.currentTime / videoPlayer.duration) * 100;
+  progressFilled.style.flexBasis = `${progressPercent}%`;
+}
+
+// Function to scrub the video when the progress bar is clicked
+function scrub(event) {
+  const scrubTime = (event.offsetX / progress.offsetWidth) * videoPlayer.duration;
+  videoPlayer.currentTime = scrubTime;
+}
+
+// Function to update the volume of the video
+function updateVolume() {
+  videoPlayer.volume = volumeInput.value;
+}
+
+// Function to update the playback speed of the video
+function updatePlaybackSpeed() {
+  videoPlayer.playbackRate = playbackSpeedInput.value;
+}
+
+// Function to skip the video forward or backward
+function skip() {
+  const skipTime = parseFloat(this.dataset.skip);
+  videoPlayer.currentTime += skipTime;
+}
+
+   
